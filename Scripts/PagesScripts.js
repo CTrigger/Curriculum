0fb42a3bb144be16743e6e23bda7233d065199ae
ctrigger <hexadecimal.exe@gmail.com>
2021-08-05 15:09:52 +0100

    Cleaning UI in certificate page, implement modal

--- Scripts/PagesScripts.js
+++ Scripts/PagesScripts.js
@@ -14,3 +14,28 @@ function OpenIconURL(url){
         '_blank'
     )
 }
+
+function ModalDataLoad(el) {
+    fetch(el)
+    .then(()=>{
+        var root = el.parentElement.parentElement.parentElement.parentElement;
+        var title = root.querySelector('.card-header > img ').getAttribute('title');
+        var content = root.querySelector('.card-body > .card-text ').innerText;
+
+        var modal = document.querySelector(el.getAttribute('data-bs-target'));
+        modal.querySelector('.modal-header > .modal-title').innerText = title;
+        modal.querySelector('.modal-body').innerText = content;
+    })
+
+}
+
+function CleanModal(el) {
+    var modal = el.parentElement.parentElement;
+        modal.querySelector('.modal-header > .modal-title').innerText = '';
+        modal.querySelector('.modal-body').innerText = '';
+    // fetch(el)
+    // .then(()=>{
+
+        
+    // })
+}
